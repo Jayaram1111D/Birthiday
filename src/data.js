e1474f4cb6754d81057efba66cd9107d3bbed60c
unknown <jayarammahesh27@gmail.com>
2021-12-09 20:51:46 +0530

    Birthiday

---
+++ src/data.js
@@ -0,0 +1,37 @@
+//eslint-disable-next-line
+export default [
+    {
+        id: 1,
+        name: 'Virat kohli',
+        age:32,
+        image: 'https://cdn.dnaindia.com/sites/default/files/styles/full/public/2021/09/17/996851-virat-kohli.jpg'
+    },
+
+    {
+        id: 2,
+        name: 'Mahesh',
+        age:46,
+        image: 'https://i.pinimg.com/originals/1c/22/02/1c2202f880b7424e9422413ffc4d1b20.jpg'
+    },
+
+    {
+        id: 3,
+        name: 'ab de villiers',
+        age:35,
+        image: 'https://images.news18.com/ibnlive/uploads/2021/07/1627310607_ab-de-villiers.jpg'
+    },
+
+    {
+        id: 4,
+        name: 'Ntr',
+        age:35,
+        image: 'https://www.filmibeat.com/ph-big/2019/05/jr-ntr_1558329150210.jpg'
+    },
+
+    {
+        id: 5,
+        name: 'Prabhas',
+        age:39,
+        image: 'https://pbs.twimg.com/profile_images/1428936441359323142/rdszNzrj_400x400.jpg'
+    }
+]
